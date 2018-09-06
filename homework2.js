7b5c83c9ae846bb42c3995e65210ae783a8a59d2
LiaHar <lia-ht@yandex.ru>
2018-09-06 13:54:07 +0400

    initial commit

---
+++ homework2.js
@@ -0,0 +1,15 @@
+
+
+function myFunction () {
+    const anun = document.getElementById("vercnelanun").value;
+    const azganun = document.getElementById("vercnelazganun").value;
+const age = anun.length + azganun.length;
+document.getElementById("inchage").innerHTML = age;
+};
+ 
+function myFunction2 () {
+    const anun = document.getElementById("vercnelanun1").value;
+    const azganun = document.getElementById("vercnelazganun1").value;
+const age = anun.length + azganun.length;
+document.getElementById("inchage2").innerHTML = age;
+};
